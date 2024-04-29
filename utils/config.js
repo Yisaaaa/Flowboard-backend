9d1533a2886393e90f4be1af8cfeee58bf323e4c
Yisaaaa <ryansanisit19@gmail.com>
2024-04-29 18:31:11 +0800

    configured to use the correct mongo uri based on node environment

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 console.log(console.log(require("dotenv").config()));
 const PORT = process.env.PORT;
 
+MONGO_URI =
+  process.env.NODE_ENV == "development"
+    ? process.env.MONGO_URI
+    : process.env.MONGO_TEST_URI;
+
 console.log(PORT);
 
-module.exports = { PORT };
+module.exports = { PORT, MONGO_URI };
