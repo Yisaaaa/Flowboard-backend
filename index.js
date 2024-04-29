f985cc4da14f8989c3a403f0d5bb057e71133809
Yisaaaa <ryansanisit19@gmail.com>
2024-04-29 18:41:08 +0800

    added connection to mongo db

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const app = require("./app");
 const config = require("./utils/config");
+const mongoose = require("mongoose");
 
-app.listen(config.PORT, () => {
-    console.log(`App listening at port ${config.PORT}`);
-});
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => {
+    console.log("Successfully connect to DB");
+    app.listen(config.PORT, () => {
+      console.log(`App listening at port ${config.PORT}`);
+    });
+  })
+  .catch((err) => console.log("failed to connect to db: ", err));
--- utils/config.js
+++ utils/config.js
@@ -1,13 +1,10 @@
 require("dotenv").config();
 
-console.log(console.log(require("dotenv").config()));
 const PORT = process.env.PORT;
 
 MONGO_URI =
-  process.env.NODE_ENV == "development"
+  process.env.NODE_ENV === "development"
     ? process.env.MONGO_URI
     : process.env.MONGO_TEST_URI;
-
-console.log(PORT);
 
 module.exports = { PORT, MONGO_URI };
