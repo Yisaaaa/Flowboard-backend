0668b43c45d0c209619593aa2734b7dadfaaf5d2
Yisaaaa <ryansanisit19@gmail.com>
2024-04-29 18:23:48 +0800

    update

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,8 @@
-const PORT = 3001;
+require("dotenv").config();
+
+console.log(console.log(require("dotenv").config()));
+const PORT = process.env.PORT;
+
+console.log(PORT);
 
 module.exports = { PORT };
